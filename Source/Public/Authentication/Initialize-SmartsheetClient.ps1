5c1f10aa69d7a71aa92f78a7b714a70e94d3454b
Skylar Ragan <skylar.ragan@smartsheet.com>
2021-01-06 14:52:52 -0800

    CHG: Update verb for smartsheet client

---
+++ Source/Public/Authentication/Initialize-SmartsheetClient.ps1
@@ -0,0 +1,72 @@
+Function Initialize-SmartsheetClient {
+<#
+    .SYNOPSIS
+        Creates a Smartsheet Client Object that you can use to make API calls and sets it to the default
+    
+    .DESCRIPTION
+        Creates a Smartsheet Client Object that you can use to make API calls and sets it to the default
+    
+    .PARAMETER AccessToken
+        A description of the AccessToken parameter.
+    
+    .PARAMETER AssumedUser
+        The email address of a member of your Org that you want to impersonate.
+        NOTE: You must have Sys Admin privileges to use this.
+    
+    .PARAMETER CustomURI
+        Use this Param if you are interacting with a custom intance or Smartsheet.
+        NOTE: Intended for internal use only
+    
+    .PARAMETER DefaultURI
+        Use this switch if you are interacting with them main Smartsheet app
+    
+    .PARAMETER Gov
+        Use this switch if you are interacting with Smartsheet Gov
+    
+    .EXAMPLE
+        PS C:\> New-SmartsheetClient -AccessToken 'll352u9jujauoqz4gstvsae05'
+    
+    .NOTES
+        Additional information about the function.
+#>
+    
+    [CmdletBinding(DefaultParameterSetName = 'DefaultURI')]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $AccessToken,
+        [string]
+        $AssumedUser,
+        [Parameter(ParameterSetName = 'CustomURI',
+                   DontShow = $true)]
+        [ValidatePattern('http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?')]
+        [String]
+        $CustomURI,
+        [Parameter(ParameterSetName = 'DefaultURI')]
+        [Switch]
+        $DefaultURI,
+        [Parameter(ParameterSetName = 'GovURI')]
+        [switch]
+        $Gov
+    )
+    
+    Process {
+        Switch ($PSCmdlet.ParameterSetName) {
+            "DefaultURI" {
+                $client = [Smartsheet.Api.SmartsheetBuilder]::new().SetAccessToken($AccessToken)
+            }
+            "GovURI" {
+                $client = [Smartsheet.Api.SmartsheetBuilder]::new().SetAccessToken($AccessToken).SetBaseURI([Smartsheet.Api.SmartsheetBuilder]::GOV_BASE_URI)
+            }
+            "CustomURI" {
+                $client = [Smartsheet.Api.SmartsheetBuilder]::new().SetAccessToken($AccessToken).SetBaseURI($CustomURI)
+            }
+        }
+        
+        If (![string]::IsNullOrWhiteSpace($AssumedUser)) {
+            [Void]$Client.SetAssumedUser($AssumedUser)
+        }
+        $script:SmartsheetClient = $Client
+    }
+}
